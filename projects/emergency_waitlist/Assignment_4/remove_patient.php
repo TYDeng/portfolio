a6e9883f584c6fbd315ea1530bdb0c7a495c2819
Wenbo <164053327+CZ2508444186@users.noreply.github.com>
2024-08-01 08:40:50 +0800

    Delete projects/emergency_waitlist directory

--- projects/emergency_waitlist/Assignment_4/remove_patient.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'db_connect.php';
-
-$id = $_POST['id'];
-
-$query = $pdo->prepare("DELETE FROM patients WHERE id = ?");
-$query->execute([$id]);
-?>
--- projects/emergency_waitlist/Assignment_4/signout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy();  // Clear the session
-header("Location: ../index.html");  // Redirect to the login page
-?>
